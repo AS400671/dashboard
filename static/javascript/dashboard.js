8c6c7a88890a31882c0929dc8c580a37064eecf5
Harold Kim <me@harold.kim>
2022-08-04 02:48:00 +0900

    feat: add icons

--- static/javascript/dashboard.js
+++ static/javascript/dashboard.js
@@ -1,3 +1,16 @@
+/*
+
+                d8                           
+        d88~\ _d88__ Y88b  / 888-~88e  888-~\
+       C888    888    Y888/  888  888b 888   
+        Y88b   888     Y8/   888  8888 888   
+         888D  888      Y    888  888P 888   
+       \_88P   "88_/   /     888-_88"  888   
+                     _/      888             
+
+                    Copyright 2022 stypr.com
+
+*/
 
 var dataLoadedComplete = false;
 var myChart = null;
@@ -588,4 +601,3 @@ const fetchDashboard = async (currentPage) => {
     };
     fetchDashboard(location.hash.slice(1));
 })();
-
