9901e8beebf22ee9e4ffe83519596179e45bb2a0
Harold Kim <me@harold.kim>
2022-08-04 19:22:45 +0900

    fix: xss, layout bugs

--- static/javascript/init.js
+++ static/javascript/init.js
@@ -12,6 +12,16 @@
 
 */
 
+const escapeHTML = (unsafe_str) => {
+    return unsafe_str
+      .replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/\"/g, '&quot;')
+      .replace(/\'/g, '&#39;')
+      .replace(/\//g, '&#x2F;')
+}
+
 (() => {
 //    activateHeader();
 })();
